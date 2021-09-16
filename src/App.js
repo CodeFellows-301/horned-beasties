fa8ca48f00da9c7fb77144272cc79dcf2e4f211d
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-09-15 18:13:08 -0700

    adjusted app.js showState -> true

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showBeast: true,
+      showBeast: false,
       beast: beastObjects[0]
     }
   };
